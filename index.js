ddf0f3f07c2f3396ab9b544aeb593f1432634383
langgames <superlanggames@gmail.com>
2024-07-29 18:54:34 +0900

    Update element bounding logic

--- index.js
+++ index.js
@@ -81,30 +81,58 @@ class Player extends EngineObject {
 
 class Box extends EngineObject {
     constructor(pos, size) {
-        super(pos, size)
+        super(pos, size, undefined, 0, new Color(0, 0, 0, 0))
         this.setCollision()
         this.mass = 0
         this.gravityScale = 0
-        this.canPass = false
+        this.renderOrder = -1
+    }
+}
+
+class ElementBox extends Box {
+    constructor(element, boundaryPos = 'bottom', color = getColor('ground')) {
+        super()
+        this.element = element
+        this.boundaryPos = boundaryPos
+        this.color = color
     }
 
     collideWithObject(object) {
-        if (this.canPass && keyWasPressed(40)) {
+        if (keyWasPressed(40) || object.velocity.y > 0) {
             return false
         }
 
-        if (this.canPass && object.velocity.y > 0) {
-            return false
-        }
         return true
     }
 
     render() {
-        this.color = this.canPass ? new Color().setHex('#8B6E4E') : new Color(0, 0, 0, 0)
-        this.renderOrder = -1
-        super.render()
+        const { x, y, width, height } = this.element.getBoundingClientRect();
+
+        switch (this.boundaryPos) {
+            case 'top':
+                this.pos = screenToWorld(vec2(x + width / 2, y));
+                break;
+            case 'bottom':
+                this.pos = screenToWorld(vec2(x + width / 2, y + height));
+                break;
+            case 'left':
+                this.pos = screenToWorld(vec2(x, y + height / 2));
+                break;
+            case 'right':
+                this.pos = screenToWorld(vec2(x + width, y + height / 2));
+                break;
+            case 'middle':
+                this.pos = screenToWorld(vec2(x + width / 2, y + height / 2));
+                break;
+            default:
+                return null;
+        }
+
+        this.size = vec2(screenToWorld(vec2(x + width, y + height)).subtract(screenToWorld(vec2(x, y))).x, 4);
+        drawRect(this.pos, this.size, this.color)
     }
 }
+
 function gameInit() {
     objectMaxSpeed = 100
     cameraScale = 1
@@ -120,15 +148,40 @@ function gameInit() {
     if (window.ontouchstart && window.ontouchend) {
         ontouchstart = ontouchend = () => { }
     }
+
+    setTimeout(() => {
+        const sizeH = vec2(mainCanvasSize.x, 10)
+        const sizeV = vec2(10, mainCanvasSize.y)
+        const top = cameraPos.add(vec2(0, mainCanvasSize.y / 2))
+        const left = cameraPos.add(vec2(-mainCanvasSize.x / 2 - 8, 0))
+        const right = cameraPos.add(vec2(mainCanvasSize.x / 2 - 24, 0))
+
+        new Box(top, sizeH)
+        new Box(left, sizeV)
+        new Box(right, sizeV)
+
+        const ground = document.querySelector('.ground')
+        if (ground)
+            new ElementBox(ground, 'top')
+
+        for (const element of [...document.querySelector('.container').children]) {
+            new ElementBox(element, 'bottom', getColor('sky-medium'))
+        }
+
+        for (const element of [...document.querySelector('.game-list').children]) {
+            if (!player) {
+                const { x, y, width } = element.getBoundingClientRect();
+                player = new Player(screenToWorld(vec2(x + width / 2, y + -32)));
+            }
+
+            new ElementBox(element, 'top')
+            new ElementBox(element, 'middle')
+            new ElementBox(element, 'bottom')
+        }
+    }, 100)
 }
 
-let postInit = true
 function gameUpdate() {
-    if (postInit) {
-        postInit = false
-        // addPlatforms()
-        drawElementBoxes()
-    }
 }
 
 function gameUpdatePost() {
@@ -143,128 +196,6 @@ function gameRenderPost() {
 // Start the LittleJS engine with our custom init function
 engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost);
 
-
-function drawElementBoxes() {
-    const mediaQuery = window.matchMedia('(max-width: 800px) and (orientation: landscape)');
-    engineObjects.forEach(o => o instanceof Box && (o.canPass = false))
-    engineObjectsDestroy()
-    player = null
-    const sizeH = vec2(mainCanvasSize.x, 10)
-    const sizeV = vec2(10, mainCanvasSize.y)
-    const bot = cameraPos.add(vec2(0, -mainCanvasSize.y / 2 - 8 + 25))
-    const top = cameraPos.add(vec2(0, mainCanvasSize.y / 2))
-    const left = cameraPos.add(vec2(-mainCanvasSize.x / 2 - 8, 0))
-    const right = cameraPos.add(vec2(mainCanvasSize.x / 2 - 24, 0))
-    new Box(bot, sizeH)
-    new Box(top, sizeH)
-    new Box(left, sizeV)
-    new Box(right, sizeV)
-    for (const element of [...document.querySelector('.game-list').children]) {
-        if (!player) {
-            const { x, y, width } = element.getBoundingClientRect();
-            player = new Player(screenToWorld(vec2(x + width / 2, y + -32)));
-        }
-        createBoxFromElement(element, 'top')
-        if (!mediaQuery.matches) {
-            createBoxFromElement(element, 'middle')
-        }
-        createBoxFromElement(element, 'bottom')
-    }
-    for (const element of [...document.querySelector('.platform-container').children]) {
-        createBoxFromElement(element, 'top')
-    }
+function getColor(name) {
+    return new Color().setHex(getComputedStyle(document.body).getPropertyValue(`--${name}`))
 }
-
-function createBoxFromElement(element, position = 'bottom') {
-    if (!element) {
-        console.error(`Element not found with selector: ${element}`);
-        return null;
-    }
-
-    const { x, y, width, height } = element.getBoundingClientRect();
-    let pos;
-
-    switch (position.toLowerCase()) {
-        case 'top':
-            pos = screenToWorld(vec2(x + width / 2, y));
-            break;
-        case 'bottom':
-            pos = screenToWorld(vec2(x + width / 2, y + height));
-            break;
-        case 'left':
-            pos = screenToWorld(vec2(x, y + height / 2));
-            break;
-        case 'right':
-            pos = screenToWorld(vec2(x + width, y + height / 2));
-            break;
-        case 'middle':
-            pos = screenToWorld(vec2(x + width / 2, y + height / 2));
-            break;
-        default:
-            console.error(`Invalid position: ${position}. Use 'top', 'bottom', 'left', or 'right'.`);
-            return null;
-    }
-
-    const size = vec2(screenToWorld(vec2(x + width, y + height)).subtract(screenToWorld(vec2(x, y))).x, 4);
-
-    const box = new Box(pos, size);
-    box.canPass = true;
-
-    return box;
-}
-
-// function addPlatforms() {
-//     const platformContainer = document.querySelector('.platform-container');
-//     platformContainer.innerHTML = ''; // Clear existing platforms
-
-//     const { bottom: gameListBottom } = document.querySelector('.game-list').getBoundingClientRect()
-
-//     const windowWidth = window.innerWidth
-//     const groundHeight = 50; // Height of the ground element
-//     const minSpaceForPlatforms = 100; // Minimum space required to add platforms
-//     const platformHeight = 20;
-//     const platformWidth = 150;
-//     const spacing = 50; // Minimum space between platforms
-
-//     const availableSpace = gameListBottom - groundHeight
-//     if (availableSpace < minSpaceForPlatforms) {
-//         return; // Don't add platforms if there's not enough space
-//     }
-
-//     const numPlatforms = Math.floor(availableSpace / spacing);
-
-//     for (let i = 0; i < numPlatforms; i++) {
-//         const platform = document.createElement('div');
-//         platform.className = 'platform';
-
-//         // Alternate left and right
-//         const left = windowWidth / 2 + (i % 2 ? 1 : -1) * platformWidth / 2 - platformWidth / 2
-
-//         let previousBottom = 0
-//         if (platformContainer.lastChild)
-//             previousBottom = parseFloat(platformContainer.lastChild.style.bottom);
-//         const bottom = previousBottom + spacing;
-
-//         platform.style.cssText = `
-//             position: absolute;
-//             width: ${platformWidth}px;
-//             height: ${platformHeight}px;
-//             left: ${left}px;
-//             bottom: ${bottom}px;
-//             background-color: var(--button);
-//             border: 2px solid var(--ground);
-//         `;
-
-//         platformContainer.appendChild(platform);
-//     }
-// }
-
-function handleResize() {
-    // requestAnimationFrame(() => {
-    // addPlatforms()
-    requestAnimationFrame(drawElementBoxes)
-    // })
-}
-
-window.addEventListener('resize', handleResize);
-window.addEventListener('orientationchange', drawElementBoxes);
